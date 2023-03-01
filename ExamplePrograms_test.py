2c757b351598ff2b630120cf963c96ac2df00c3f
CHETTI SAI SRINIVAS <srinu.samm@gmail.com>
2023-02-28 16:29:44 -0800

    added fixtures to test

--- ExamplePrograms_test.py
+++ ExamplePrograms_test.py
@@ -1,11 +1,18 @@
 import ExamplePrograms
+import pytest
 
-def test_power():
-    output = ExamplePrograms.power([2, 4, 8])
+#Input for the first 2 tests
+@pytest.fixture
+def inputPower():
+    input = [2, 4, 8]
+    return input
+
+def test_power(inputPower):
+    output = ExamplePrograms.power(inputPower)
     assert output == [4, 16, 64]
 
-def test_aveGrades():
-    output = ExamplePrograms.aveGrades([2, 4, 8])
+def test_aveGrades(inputPower):
+    output = ExamplePrograms.aveGrades(inputPower)
     assert output == 14 // 3
 
 def test_reverseString():
@@ -16,10 +23,16 @@ def test_twoSum():
     output = ExamplePrograms.twoSum([2, 7, 11, 15], 9)
     assert output == [1, 0]
 
-def test_maxProfit():
-    output = ExamplePrograms.maxProfit([7,1,5,3,6,4])
+#Input for the next 2 tests
+@pytest.fixture
+def inputList():
+    input = [7,1,5,5,3,6,4]
+    return input
+
+def test_maxProfit(inputList):
+    output = ExamplePrograms.maxProfit(inputList)
     assert output == 5
 
-def test_containsDuplicates():
-    output = ExamplePrograms.containsDulicates([1,2,3,3,5,6])
+def test_containsDuplicates(inputList):
+    output = ExamplePrograms.containsDulicates(inputList)
     assert output == True
